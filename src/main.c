cb3d78d100d2526c4743c88a5a044207d490279c
CoelhoEduardo <educs98@outlook.com>
2024-11-28 19:37:14 -0300

    check if all args was a digit

--- src/main.c
+++ src/main.c
@@ -6,17 +6,45 @@
 /*   By: ecoelho- <ecoelho-@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 19:26:59 by ecoelho-          #+#    #+#             */
-/*   Updated: 2024/11/28 19:29:52 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2024/11/28 19:35:04 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char **argv) {
-  if (argc != 6 && argc != 5)
+
+int ft_is_digit(int c) {
+  if (c >= '0' && c <= '9')
+    return (1);
+  return (0);
+}
+
+int has_arg_digit(const char *arg) {
+  int i;
+
+  i = 0;
+  while(arg[i] != '\0')
   {
-      printf("The number of args is not valid");
+    if (!ft_is_digit(arg[i])) {
+      printf("The arg is not valid, please check");
       exit(1);
+    }
+    i++;
+  }
+
+  return (0);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 6 && argc != 5) {
+    printf("The number of args is not valid");
+    exit(1);
+  }
+
+  int i;
+  i = 1;
+  while (i < argc) {
+    has_arg_digit(argv[i++]);
   }
 
   return (0);
