b5e18ca7b84b80019d56487b70e10e0f1f51980a
Eduardo Coelho Dos Santos <educs98@outlook.com>
2024-12-09 11:22:00 -0300

    start routines and threads, first think

---
+++ src/routines.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routines.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 11:08:55 by ecoelho-          #+#    #+#             */
+/*   Updated: 2024/12/09 11:21:02 by ecoelho-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	get_status(char *str, t_philo *philo, int id)
+{
+	size_t	time;
+
+	pthread_mutex_lock(philo->write_lock);
+	time = get_current_time() - philo->start_time;
+	if (!dead_loop(philo))
+		printf("%zu %d %s\n", time, id, str);
+	pthread_mutex_unlock(philo->write_lock);
+}
+
+int	dead_loop(t_philo *philo)
+{
+	pthread_mutex_lock(philo->dead_lock);
+	if (*philo->dead == 1)
+		return (pthread_mutex_unlock(philo->dead_lock), 1);
+	pthread_mutex_unlock(philo->dead_lock);
+	return (0);
+}
+
+void	is_think(t_philo *philo)
+{
+	get_status("is thinking", philo, philo->id);
+}
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 20:01:44 by ecoelho-          #+#    #+#             */
-/*   Updated: 2024/12/09 11:03:53 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2024/12/09 11:20:55 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,12 @@ size_t	get_current_time(void)
 	return (time.tv_sec * 1000 + time.tv_sec / 1000);
 }
 
-int ft_usleep(size_t milliseconds) {
+int	ft_usleep(size_t milliseconds)
+{
 	size_t	start;
 
 	start = get_current_time();
 	while ((get_current_time() - start) < milliseconds)
 		usleep(500);
-	
 	return (0);
 }
